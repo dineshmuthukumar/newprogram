4830efae09e0d8a1af8ab09c9ee0b85cc4014603
muthukumar <dineshmuthukumar.g@kenhike.com>
2018-08-10 18:39:49 +0530

    form_operations

---
+++ newpage.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Entered Details is" ."<br>";
+echo "Firstname is"." ".$_POST["Firstname"]."<br>";
+echo "Lastname is"." ".$_POST["Lastname"]."<br>";
+echo "upload File is"." " .$_POST["myFile"]."<br>";
+echo "Gender is"." ".$_POST["gender"]."<br>";
+echo "Address is"." ".$_POST["Address"]."<br>";
+echo "City is"." ".$_POST["City"]."<br>";
+echo "State is"." ".$_POST["State"]."<br>";
+echo "Country is"." ".$_POST["Country"]."<br>";
+echo "Phone_no is"." ".$_POST["Phone_no"]."<br>";
+echo "Hobbies was"." ".$_POST["Hobbies"]."<br>";
+echo "Email_id is"." ".$_POST["Email_id"]."<br>";
+echo "Your Password is"." ".$_POST["Password"];
+?>
